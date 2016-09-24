e7a3451f8acf5148a508305a092132532ff96730
Fred <Fred@FRED-PC>
2016-09-24 18:29:33 +1000

    Added Layout SignIn and Register pages

--- src/main/java/com/ceaot/controller/ItemController.java
+++ src/main/java/com/ceaot/controller/ItemController.java
@@ -23,4 +23,75 @@ public class ItemController {
     @Inject
     private ItemEJB itemEJB;
     
+    private String category; // Stores item category.
+    private Long itemNum; //Stores Item Number
+    private String searchString; // Holds the search string from search pages
+    
+    
+    //TO DO search by Item Number
+    private String searchByNum(){
+        return null;
+    }
+    
+    //TO DO Search by description
+    private String searchByDes(){
+        return null;
+    }
+
+    /**
+     * @return the itemEJB
+     */
+    public ItemEJB getItemEJB() {
+        return itemEJB;
+    }
+
+    /**
+     * @param itemEJB the itemEJB to set
+     */
+    public void setItemEJB(ItemEJB itemEJB) {
+        this.itemEJB = itemEJB;
+    }
+
+    /**
+     * @return the category
+     */
+    public String getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    /**
+     * @return the itemNum
+     */
+    public Long getItemNum() {
+        return itemNum;
+    }
+
+    /**
+     * @param itemNum the itemNum to set
+     */
+    public void setItemNum(Long itemNum) {
+        this.itemNum = itemNum;
+    }
+
+    /**
+     * @return the searchString
+     */
+    public String getSearchString() {
+        return searchString;
+    }
+
+    /**
+     * @param searchString the searchString to set
+     */
+    public void setSearchString(String searchString) {
+        this.searchString = searchString;
+    }
+    
 }
